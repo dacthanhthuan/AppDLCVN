b74bb1c04b8f4d15e10d2151ef4bbe0ca70ec98f
THUAN DAC <dtthuan.2003@gmail.com>
2023-05-16 08:54:48 +0700

    bottom

---
+++ src/screens/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles";
+import {View} from 'react-native'
+const Home = () =>{
+    return(
+        <View>
+                
+        </View>
+    )
+}
+
+export default Home;
